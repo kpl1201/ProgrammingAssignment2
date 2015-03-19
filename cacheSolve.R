bd53fd75be09da7e4f85f59218f08f880ad772cc
kpl1201 <kapil_gurjar1201@rediffmail.com>
2015-03-19 10:41:49 +0530

    Create cacheSolve.R
    cacheSolve.R Added by Kapil

---
+++ cacheSolve.R
@@ -0,0 +1,13 @@
+cacheSolve <- function(x, ...) {
+  
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+    
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
+}
