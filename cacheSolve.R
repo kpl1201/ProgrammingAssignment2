ad28c9f796dd7af133fb0ee6f25d4e401291f7dd
kpl1201 <kapil_gurjar1201@rediffmail.com>
2015-03-19 11:05:40 +0530

    Update cacheSolve.R
    updated with comments

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,12 @@
 cacheSolve <- function(x, ...) {
   
-  m<-x$getmatrix()
-  if(!is.null(m)){
+  m<-x$getmatrix() ##Checking the Matrix value
+  if(!is.null(m)){ ##If present, get inverse from cache
     message("getting cached data")
     return(m)
     
   }
+  ## IF not available get as new and set in cache
   matrix<-x$get()
   m<-solve(matrix, ...)
   x$setmatrix(m)
